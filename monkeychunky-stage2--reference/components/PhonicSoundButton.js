8c220baae11b04802f31d5ca67ef21bc99eb4f68
v271 <62334205+v271@users.noreply.github.com>
2020-11-30 16:45:09 +0530

    Add files via upload

---
+++ monkeychunky-stage2--reference/components/PhonicSoundButton.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import {Audio} from 'expo-av'
+
+export default class PhonicSoundButton extends React.Component{
+
+  playSound = async (soundChunk) =>{
+    var soundlink = 'https://whitehatjrcontent.s3.ap-south-1.amazonaws.com/phones/'+soundChunk+'.mp3'
+    await Audio.Sound.createAsync(
+      {uri:soundlink},
+      {shouldPlay:true}
+    ) 
+  }
+
+  render(){
+    return(
+     <TouchableOpacity style= {styles.chunkButton}
+     onPress ={()=>{
+       this.playSound(this.props.soundChunk)
+     }}
+     >
+    <Text  style = {styles.displayText}>{this.props.wordChunk}</Text> 
+     </TouchableOpacity>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  
+  
+  displayText: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: 'white',
+  },
+  chunkButton: {
+    width: '60%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: 10,
+    margin: 5,
+    backgroundColor: 'red',
+  },
+});
